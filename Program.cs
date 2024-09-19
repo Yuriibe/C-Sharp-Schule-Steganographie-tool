473fd9643ff329ae67c54907f2a2b671f24280c1
Yuriibe <direxpvp@gmail.com>
2024-09-19 14:17:25 +0200

    init commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Load the image
+        Bitmap image = new Bitmap("hund.png");
+
+        // Loop through all rows and columns to change each pixel's color
+        for (int y = 0; y < image.Height; y++)
+        {
+            for (int x = 0; x < image.Width; x++)
+            {
+                Color pixel = image.GetPixel(x, y);
+
+
+                image.SetPixel(x, y, Color.FromArgb(255, 200, 150));
+
+
+                Console.WriteLine($"Modified Pixel at ({x}, {y}): Original Color = {pixel}, New Color = (255, 200, 150)");
+            }
+        }
+
+
+        image.Save("output.png");
+
+
+        image.Dispose();
+    }
+}
